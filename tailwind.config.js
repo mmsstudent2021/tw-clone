ef1a757d84e390f7f03cd5a05126e80fc32d65e2
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-06-01 13:37:13 +0630

    font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        'poppins' : ["Poppins", "sans-serif"],
+        'paduak' : ["Padauk", "sans-serif"],
+        'poppins-padauk' : ["Poppins","Padauk", "sans-serif"]
+      },
+    },
   },
   plugins: [],
 }
